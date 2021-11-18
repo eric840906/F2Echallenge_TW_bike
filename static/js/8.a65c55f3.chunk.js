73ec7486ea659bc50a35a90768a78c96089b986a
eric840906 <65453270+eric840906@users.noreply.github.com>
2021-11-18 07:42:09 +0000

    Deploying to gh-pages from @ eric840906/F2Echallenge_TW_bike@804dfe6b44f7a22d1c225bdf1c1476d7589f00aa 🚀

--- static/js/8.a65c55f3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpbike=this.webpackJsonpbike||[]).push([[8],{173:function(e,t,r){"use strict";r.r(t);var a=r(64),s=r(6);t.default=function(){return Object(s.jsx)(s.Fragment,{children:Object(s.jsx)(a.i,{gridGap:7,my:14,children:Object(s.jsx)(a.f,{w:{base:"95%",md:"80%"},templateColumns:{base:"repeat(1, 1fr)",md:"repeat(2, 1fr)",lg:"repeat(3, 1fr)"},gridGap:{base:3,md:5}})})})}}}]);
-//# sourceMappingURL=8.a65c55f3.chunk.js.map
